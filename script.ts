4c62a390f30a10fceae39d23eeba719a65d80c05
GridBoost <james@gridboost.io>
2024-08-25 17:14:54 -0400

    Updated formatting with tailwind and typescript

--- script.ts
+++ script.ts
@@ -1,32 +1,34 @@
-"use strict";
-function copyToClipboard(elementId) {
-    var el = document.getElementById(elementId);
+function copyToClipboard(elementId: string): void {
+    const el = document.getElementById(elementId);
     if (el) {
-        var text = el.textContent;
+        const text = el.textContent;
         if (text) {
-            navigator.clipboard.writeText(text).then(function () {
+            navigator.clipboard.writeText(text).then(() => {
                 console.log('Copying to clipboard was successful!');
                 showCopiedMessage(elementId);
-            }, function (err) {
+            }, (err) => {
                 console.error('Could not copy text: ', err);
             });
         }
     }
 }
-function showCopiedMessage(elementId) {
-    var button = document.querySelector("button[onclick=\"copyToClipboard('".concat(elementId, "')\"]"));
+
+function showCopiedMessage(elementId: string): void {
+    const button = document.querySelector(`button[onclick="copyToClipboard('${elementId}')"]`);
     if (button) {
-        var originalText_1 = button.textContent;
+        const originalText = button.textContent;
         button.textContent = 'Copied!';
         button.classList.remove('bg-green-500', 'hover:bg-green-600');
         button.classList.add('bg-blue-500', 'hover:bg-blue-600');
-        setTimeout(function () {
-            button.textContent = originalText_1;
+        
+        setTimeout(() => {
+            button.textContent = originalText;
             button.classList.remove('bg-blue-500', 'hover:bg-blue-600');
             button.classList.add('bg-green-500', 'hover:bg-green-600');
         }, 2000);
     }
 }
-document.addEventListener('DOMContentLoaded', function () {
+
+document.addEventListener('DOMContentLoaded', () => {
     Prism.highlightAll();
 });
